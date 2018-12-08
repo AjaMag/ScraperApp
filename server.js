7840c1b9f96ffdae844db5d49ca87881320c40b3
Phoenix <aja.magdaleno@gmail.com>
2018-12-08 13:17:27 -0800

    server and initial folder structures

--- server.js
+++ server.js
@@ -25,6 +25,4 @@ app.use(express.static("public"));
 // })
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });

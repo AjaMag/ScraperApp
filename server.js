380e0e528e102394f731ce8633110ef1fbfcb13c
Phoenix <aja.magdaleno@gmail.com>
2018-12-10 09:55:25 -0800

    first axios call and style

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //All dependencies to log, render, and fetch data, etc..
-const express = require('express')
-const cheerio = require('cheerio')
-const axios = require('axios')
-const logger = require('morgan')
-const mongoose = require('mongoose')
+const express = require("express");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var app = express()
+var app = express();
 
 // Require all models
 // var db = require("./models");
@@ -18,6 +18,21 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+//routes
+
+axios
+  .get("http://reddit.com/")
+  .then(r => {
+    const $ = cheerio.load(r.data);
+    const headers = $(".imors3-0.iuScIP").each((i, elem) => console.log($(elem).text()))
+  })
+  .catch(e => console.log(e));
+
+// Headline - the title of the article
+// Summary - a short summary of the article
+// URL - the url to the original article
+// Feel free to add more content to your database (photos, bylines, and so on).
+
 // const db = mongo('reddit')
 // db.post.insert(
 //     {entry: i+1, header: $(elem.html)}, () => {
@@ -25,4 +40,7 @@ app.use(express.static("public"));
 // })
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb://localhost/mongoHeadlines",
+  { useNewUrlParser: true }
+);
